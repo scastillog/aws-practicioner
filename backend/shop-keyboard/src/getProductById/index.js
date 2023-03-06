cce2148a643388eda3a40adf65a10b1dbde94585
Sebastian <scastillogpr@gmail.com>
2023-03-05 23:48:18 -0500

    task-4

--- backend/shop-keyboard/src/getProductById/index.js
+++ backend/shop-keyboard/src/getProductById/index.js
@@ -1,16 +1,45 @@
+import AWS from "aws-sdk";
 import { successResponse } from "../utils/successResponse";
 import { errorResponse } from "../utils/errorResponse";
 
-import mock from "../mock.json";
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 export const getProductById = async (event) => {
   const { pathParameters } = event;
 
-  const getProduct = mock.find((item) => item.id === pathParameters.productId);
+  console.log(pathParameters);
 
-  if (getProduct) {
-    return successResponse(getProduct);
+  const paramsProduct = {
+    ExpressionAttributeValues: { ":i": pathParameters.productId },
+    KeyConditionExpression: "id = :i",
+    TableName: process.env.PRODUCTS_TABLE,
+  };
+
+  const paramsStocks = {
+    ExpressionAttributeValues: { ":i": pathParameters.productId },
+    KeyConditionExpression: "id = :i",
+    TableName: process.env.STOCKS_TABLE,
+  };
+
+  let product;
+  let stock;
+  try {
+    product = await dynamoDb.query(paramsProduct).promise();
+  } catch (error) {
+    return errorResponse(`Something wrong ${error}`);
   }
 
-  return errorResponse("Not Found");
+  try {
+    stock = await dynamoDb.query(paramsStocks).promise();
+  } catch (error) {
+    return errorResponse(`Something wrong ${error}`);
+  }
+
+  console.log(product);
+  console.log(stock);
+
+  if (product && stock) {
+    product.Items[0].count = stock.Items[0].count;
+    return successResponse(product.Items[0]);
+  }
 };
---
+++ tasks/TASK_04.md
@@ -0,0 +1,7 @@
+# TASK - 04
+
+Connection of lambdas to DynamoDB
+
+[Get Product List](https://6dy0wgbn9i.execute-api.us-east-1.amazonaws.com/dev/products)
+[Get a Product ](https://6dy0wgbn9i.execute-api.us-east-1.amazonaws.com/dev/products/1)
+[Create a Product - Post ](https://6dy0wgbn9i.execute-api.us-east-1.amazonaws.com/dev/products)
